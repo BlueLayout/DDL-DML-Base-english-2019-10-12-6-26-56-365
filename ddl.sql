7fa95f94382bbe85687f0a7b14a58b53204f24a2
Blackzoom <991476256@qq.com>
2020-07-03 17:38:36 +0800

    ddl and dml

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use student_examination_sys;
 -- Create a database
-
+create database student_examination_sys;
 -- Create the database of the designated character set
-
+CREATE DATABASE student_examination_sys DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
 -- Display the creation information fo the database
-
+show create table `student`;
 -- Revise the codes of the database
-
+alter database student_examination_sys character set utf8;
 -- Delete a database
-
+drop database `student`;
 -- **Table level**
 -- Revise table name
-
+rename table oldName to newName
 -- Revise the field's data type
-
+alter table student modify column name varchar(20);
 -- Revise field name
-
+alter table student change name newName varchar(100);
 -- Add field
-
+alter table student add address varchar(255);
 -- Delete field
-
+alter table student drop name;
 -- Revise the table's storage engine
-
--- Delete the table's foreign key restriant
-
+alter table student engine=InnoDB;
+-- Dtelete the table's foreign key restrian
+alter table student drop foreign key name;
 -- Delete a table
+drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into student(id,name,age,sex) values(003,'Mike',19,'male');
 -- Revise record
-
+update student set age = '19' where id = '001';
 --  Delete record
-
+delete from student where id = '001';
 -- Search record
+select * from student;
